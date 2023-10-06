691be7c9045664961ac41891724c0c60560641d1
estrallator <manubazlo@gmail.com>
2023-10-06 11:35:40 +0200

    Android app. First version, added speedometer functionality and chrono

---
+++ ControlLineCompanion2/app/src/main/java/com/mbl/controllinecompanion/Speedo.java
@@ -0,0 +1,80 @@
+package com.mbl.controllinecompanion;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Calendar;
+import java.util.Date;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Speedo extends AppCompatActivity {
+
+    TextView btn_start, btn_lap, txt_speed;
+    boolean isRunning = false;
+    boolean timeIsSet = false;
+    long currentTime ;
+    long cableLength = 20;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.speedo);
+
+        Chronometer chrono = (Chronometer) findViewById(R.id.txt_chrono);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_lap = findViewById(R.id.btn_lap);
+        txt_speed = findViewById(R.id.txt_speed);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isRunning){
+                    chrono.stop();
+                    isRunning = false;
+                    btn_start.setText("Start");
+                }
+                else {
+                    chrono.setBase(SystemClock.elapsedRealtime());
+                    chrono.start();
+                    isRunning = true;
+                    btn_start.setText("Stop");
+                }
+            }
+        });
+
+        btn_lap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!timeIsSet ) {
+                    currentTime = chrono.getTimeElapsed();
+                    timeIsSet = true;
+                    return;
+                }
+
+                long lapTimeMillis = chrono.getTimeElapsed() - currentTime;
+                double lapTimeSecs = (double)lapTimeMillis/1000.0;
+                double distanceMeters = 2 * 3.1416 * cableLength;
+
+
+                Log.d("Distance: ", String.valueOf(distanceMeters));
+                Log.d("Lap time", String.valueOf(lapTimeMillis));
+                Log.d("Lap time secs", String.valueOf(lapTimeSecs));
+
+                double speedMS = distanceMeters /  lapTimeSecs;
+
+                Log.d("speed m/s", String.valueOf(speedMS));
+                Log.d("Speed km/h", String.valueOf(speedMS * 3.6));
+                txt_speed.setText(String.format("%.1f", speedMS * 3.6));
+                currentTime = chrono.getTimeElapsed();
+
+            }
+        });
+    }
+}
